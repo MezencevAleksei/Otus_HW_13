b50661e60a8ac8bd61a42f707f79926528e0c4d9
Mezencev Aleksei <mezencev_aleksey@bk.ru>
2019-10-06 16:12:20 +0300

    Initial Commit

---
+++ Otus_HW_13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Otus_HW_13
+//
+//  Created by alex on 06/10/2019.
+//  Copyright © 2019 Mezencev Aleksei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
